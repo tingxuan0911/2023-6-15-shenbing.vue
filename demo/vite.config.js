c665d94fecdb7017a4426368bbd43ff75aec4be9
tingxuan0911 <121272572+tingxuan0911@users.noreply.github.com>
2024-06-26 16:51:33 +0800

    Update vite.config.js

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"/shenbing.vue",
+  base:"/2023-6-15-shenbing.vue",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
